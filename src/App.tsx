ced9144ce85c0dcff5dd8abd52e51d0c204bb4c1
[dyad] <git@dyad.sh>
2025-10-18 20:37:18 +0530

    [dyad] Fixing a recurring syntax error - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -187,7 +187,7 @@ function App() {
               element={
                 <AdminLayout>
                   <ManageLevelsPage />
-                </Layout>
+                </AdminLayout>
               }
             />
             <Route
